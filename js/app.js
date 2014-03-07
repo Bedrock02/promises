eaf945b4fc9bd2f3e45ddcfc24085c5920cdc75c
BJK <bernie@gowatchit.com>
2014-03-07 17:26:57 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,52 @@
+var BASE = "https://api.github.com"
+var EMBER_REPO = "/emberjs/ember.js"
+var REPO = BASE + "/repos" + EMBER_REPO
+var COLLABS = REPO + "/collaborators"
+var COMMIT = REPO + "/commits"
+var SEARCH = BASE + "/search/repositories?q=ember"
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.route("collaborators");
+  this.route("last_commit")
+});
+
+App.CollaboratorsRoute = Ember.Route.extend({
+	model: function() {
+		return [];
+	},
+ 	setupController: function(controller, model) {
+    $.get(COLLABS, function(data) {
+     controller.set('model', data);
+    })
+  }
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
+
+App.LastCommitRoute = Ember.Route.extend({
+  model: function() {return {}},
+  setupController: function(controller, model) {
+    //search git
+    $.get(SEARCH, function(data) {
+      var repo_name = data.items[0].full_name
+      repo_url = BASE + "/repos/" + repo_name + "/commits"
+      $.get(repo_url, function(commits) 
+      {
+        controller.set('model', commits[0])
+      });  
+    })
+  },
+  actions: {
+    submit: function() {alert('hi')
+    return false}
+  }
+});
+
+LastCommitController = Ember.ObjectController.extend({});
+
